916139601c6c93dc3997a4c32341df3556c65f12
SeoHyun Ahn <ahn67@purdue.edu>
2018-05-22 11:23:59 -0400

    Add Sidebar, NoteList, and Noteform and style app

--- src/Main.js
+++ src/Main.js
@@ -1,8 +1,15 @@
 import React from 'react'
+ import Sidebar from './Sidebar'
+ import NoteList from './NoteList'
+ import NoteFrom from './NoteForm'
 
 const Main =  () => {
     return (
-    <div className="Main">Main</div>
+    <div className="Main">
+        <Sidebar />
+        <NoteList />
+        <NoteFrom />
+    </div>
 )
 }
 
---
+++ src/NoteForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NoteFrom = () => {
+    return (
+        <div className = "NoteForm">
+        NoteForm
+        </div>
+    )
+}
+
+export default NoteFrom
