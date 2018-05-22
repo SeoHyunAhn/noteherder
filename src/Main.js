fd9a913941f48a7b9eaee92eeb428f96f7d08408
SeoHyun Ahn <ahn67@purdue.edu>
2018-05-22 11:44:59 -0400

    Import images and add JSX for Sidebar

--- src/Main.js
+++ src/Main.js
@@ -1,23 +1,23 @@
 import React from 'react'
- import Sidebar from './Sidebar'
- import NoteList from './NoteList'
- import NoteFrom from './NoteForm'
+import Sidebar from './Sidebar'
+import NoteList from './NoteList'
+import NoteFrom from './NoteForm'
 import { height } from 'window-size';
 
-const Main =  () => {
+const Main = () => {
     return (
-    <div className="Main">
-        <Sidebar />
-        <NoteList />
-        <NoteFrom />
-    </div>
-)
+        <div className="Main">
+            <Sidebar />
+            <NoteList />
+            <NoteFrom />
+        </div>
+    )
 }
 
 const style = {
-    display:'flex', 
-    height:'100vh',
-    alignItems:'stretch', 
+    display: 'flex',
+    height: '100vh',
+    alignItems: 'stretch',
 }
 
 export default Main
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,8 +1,30 @@
 import React from 'react'
-const Sidebar =  () => {
+import quill from './quill.svg'
+import newIcon from './new.png'
+import newHover from './new-hover.png'
+
+const Sidebar = () => {
     return (
-    <div className="Sidebar">Sidebar</div>
-)
+        <div className="Sidebar">
+            <div className="logo">
+                <img src={quill} alt="Noteherder" />
+            </div>
+            <a href="/notes">
+                <img
+                    src={newHover}
+                    alt="New Note" />
+                <img
+                    src={newIcon}
+                    alt="New Note" />
+
+            </a>
+            <div className="SignOut">
+            <button>
+              <i className="fa fa-sign-out"></i>
+            </button>
+          </div>
+        </div>
+    )
 }
 
 export default Sidebar
